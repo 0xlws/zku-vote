6cc0207f7a4451f2c54099d27fa8e41a8a7ff7bf
vrgllws <vrglbznz@gmail.com>
2022-07-06 11:47:54 +0200

    .

--- contexts/PageContext.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-const contextDefaultValues = {
-  page: "1",
-  setPage: (page: string) => {},
-};
-
-export const PageContext = createContext(contextDefaultValues);
